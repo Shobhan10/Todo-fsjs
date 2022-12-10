48181d70e3e38e59b26dfa47126f6cc6e7ec8e41
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2022-12-10 19:48:19 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const addButton = document.querySelector(".addButton");
+const container = document.querySelector(".container");
+
+addButton.addEventListener("click", () => {
+  const input = document.querySelector(".input").value;
+  console.log(input);
+});
