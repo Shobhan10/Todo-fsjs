14f42b99dde0d4bab2c9c87179696eb5ba58d5b0
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2022-12-13 23:41:05 +0530

    minor add button fix

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ const addButton = document.querySelector(".addButton");
 const container = document.querySelector(".container");
 
 addButton.addEventListener("click", () => {
-  const input = document.querySelector(".input").value;
-  console.log(input);
+  let input = document.querySelector(".input").value;
 
-  const item = document.createElement("div");
-  item.classList.add("item");
-  container.append(item);
+  if (input) {
+    const item = document.createElement("div");
+    item.classList.add("item");
+    container.append(item);
 
-  const itemInput = document.createElement("div");
-  itemInput.classList.add("item_input");
-  itemInput.textContent = input;
-  item.append(itemInput);
+    const itemInput = document.createElement("div");
+    itemInput.classList.add("item_input");
+    itemInput.textContent = input;
+    item.append(itemInput);
 
-  const editButton = document.createElement("button");
-  editButton.classList.add("editButton");
-  editButton.textContent = "EDIT";
-  item.append(editButton);
+    const editButton = document.createElement("button");
+    editButton.classList.add("editButton");
+    editButton.textContent = "EDIT";
+    item.append(editButton);
 
-  const deleteButton = document.createElement("button");
-  deleteButton.classList.add("deleteButton");
-  deleteButton.textContent = "DELETE";
-  item.append(deleteButton);
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("deleteButton");
+    deleteButton.textContent = "DELETE";
+    item.append(deleteButton);
+
+    document.querySelector(".input").value = "";
+  }
 });
