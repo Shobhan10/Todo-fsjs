0e4357e2eb7617f165d32686a9bf1d8e302c631d
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2022-12-14 23:39:41 +0530

    edit button applied but buggy

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const addButton = document.querySelector(".addButton");
 const container = document.querySelector(".container");
+const inputDiv = document.querySelector(".input_div");
 
-addButton.addEventListener("click", () => {
+const addEditButton = document.createElement("button");
+addEditButton.classList.add("addEditButton");
+addEditButton.textContent = "EDIT";
+inputDiv.appendChild(addEditButton);
+addEditButton.style.display = "none";
+
+function createTodo() {
   let input = document.querySelector(".input").value;
 
   if (input) {
@@ -26,4 +33,32 @@ addButton.addEventListener("click", () => {
 
     document.querySelector(".input").value = "";
   }
-});
+  const todos = document.querySelectorAll(".item");
+  console.log(todos);
+
+  // for (let todo = 0; todo < todos.length; todo++) {
+  //   todos[todo].addEventListener("click", () => console.log("clicked"));
+  // }
+
+  const editButton = document.querySelectorAll(".editButton");
+  const itemInput = document.querySelectorAll(".item_input");
+
+  for (let todo = 0; todo < todos.length; todo++) {
+    editButton[todo].addEventListener("click", () => {
+      document.querySelector(".input").value = itemInput[todo].innerText;
+
+      addButton.style.display = "none";
+
+      addEditButton.style.display = "block";
+
+      addEditButton.addEventListener("click", () => {
+        itemInput[todo].innerText = document.querySelector(".input").value;
+        addEditButton.style.display = "none";
+        addButton.style.display = "block";
+        document.querySelector(".input").value = "";
+      });
+    });
+  }
+}
+
+addButton.addEventListener("click", createTodo);
