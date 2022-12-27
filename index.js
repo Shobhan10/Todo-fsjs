78c4ed4448e9de3d863e7123357e204b4a9e4a25
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2022-12-27 21:46:46 +0530

    edit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const addButton = document.querySelector(".addButton");
 const container = document.querySelector(".container");
 const inputDiv = document.querySelector(".input_div");
+const inputEl = document.querySelector(".input");
 
-// const addEditButton = document.createElement("button");
-// addEditButton.classList.add("addEditButton");
-// addEditButton.textContent = "EDIT";
-// inputDiv.appendChild(addEditButton);
-// addEditButton.style.display = "none";
+const addEditButton = document.createElement("button");
+addEditButton.classList.add("addEditButton");
+addEditButton.textContent = "EDIT";
+inputDiv.appendChild(addEditButton);
+addEditButton.style.display = "none";
 
 const previousTask = JSON.parse(localStorage.getItem("myTodo"));
-
 if (!previousTask) {
   myTodo = [];
 } else {
@@ -21,15 +21,15 @@ showTodo();
 addButton.addEventListener("click", createTodo);
 
 function createTodo() {
-  let input = document.querySelector(".input").value;
+  let input = inputEl.value.trim();
 
   if (input) {
-    myTodo.push(input.trim());
+    myTodo.push(input);
   }
 
   localStorage.setItem("myTodo", JSON.stringify(myTodo));
 
-  document.querySelector(".input").value = "";
+  inputEl.value = "";
 
   showTodo();
 }
@@ -37,11 +37,11 @@ function createTodo() {
 function showTodo() {
   let todoEl = "";
 
-  myTodo.forEach((todo) => {
+  myTodo.forEach((todo, index) => {
     todoEl += `
         <div class="item">
           <div class="item_input">${todo}</div>
-          <button class="editButton">EDIT</button>
+          <button class="editButton" onClick="editTodo(${index})">EDIT</button>
           <button class="deleteButton">DELETE</button>
         </div>
     `;
@@ -49,3 +49,27 @@ function showTodo() {
 
   container.innerHTML = todoEl;
 }
+
+let indexVal;
+
+function editTodo(index) {
+  inputEl.value = myTodo[index];
+  indexVal = index;
+  addButton.style.display = "none";
+  addEditButton.style.display = "block";
+}
+
+addEditButton.addEventListener("click", () => {
+  let input = inputEl.value.trim();
+
+  if (input) {
+    myTodo[indexVal] = input;
+  }
+
+  addEditButton.style.display = "none";
+  addButton.style.display = "block";
+  inputEl.value = "";
+
+  localStorage.setItem("myTodo", JSON.stringify(myTodo));
+  showTodo();
+});
